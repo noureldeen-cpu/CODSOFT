40aeb91c071ffeba005f57fa117e921858866e9d
Eng. Nour El-Deen Mahmoud Abd El-Baseer <noureldeen242005@gmail.com>
2024-07-15 14:03:46 +0300

    Upload the first task

---
+++ NUMBER_GAME/NumberGame.java
@@ -0,0 +1,11 @@
+/* 
+*Create a simple number guessing game in Java.
+The program should generate a random number between 1 and 100,
+and the user should have a limited number of attempts to guess the correct number.
+Additionally, the program should keep track of the number of attempts made by the user and display it after each guess.
+*/
+public class NumberGame {
+    public static void main(String[] args) {
+        Start.start();
+    }
+}
